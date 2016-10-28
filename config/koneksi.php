6db261f6367778bdf1437f8cd091f22d12ee8563
asus <asus@simatupang>
2016-10-28 15:05:07 +0700

    program pertama

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,3 +3,19 @@
 // irwan nauli
 
 
+$server = "localhost" ;
+$user = "root";
+$pass = "";
+$db = "lesson";
+
+$koneksi = mysql_connect($server = "localhost", $user="root", $pass="", $db="lesson") ;
+
+if ($koneksi) {
+    
+    echo "ditemukan";
+}
+else {
+    "anda belum cukup umur";
+}
+
+
